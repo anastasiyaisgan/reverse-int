7a8316d74ca51c1238d63283b7ed855325c1d518
anastasiyaisgan <138139346+anastasiyaisgan@users.noreply.github.com>
2024-08-20 20:53:39 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,3 @@
 module.exports = function reverse (n) {
-  reverseNumber =  parseInt(Math.abs(n).toString().split('').reverse().join(''), 10);
-  return n<0? -reverseNumber : reverseNumber;
+  return parseInt(Math.abs(n).toString().split('').reverse().join(''), 10);
 }
